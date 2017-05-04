39d4f1002dc7bea6c4b05a5b7d67b103b9dd5d1f
Heather Smith <hsmith.02@outlook.com>
2017-05-04 15:50:20 -0700

    checkpoint-14 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502161729) do
+ActiveRecord::Schema.define(version: 20170504223136) do
 
-  create_table "errors", force: :cascade do |t|
-    t.string   "name"
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
