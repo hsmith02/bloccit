0e014ed0060f1a40a63abbc8dc2a904d0e403f2d
Heather Smith <hsmith.02@outlook.com>
2017-05-19 16:08:13 -0700

    checkpoint-28 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,9 +38,16 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'hsmith.02@outlook.com',
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
