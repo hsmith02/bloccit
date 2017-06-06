096a73ceec119daa43b5951c692a742c37144f82
Heather Smith <hsmith.02@outlook.com>
2017-06-06 16:16:43 -0700

    assignment-32 completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def show_collection_or_message(collection)
+    if collection.any?
+      render collection
+    else
+      "There ain't no items in that collection."
+    end
+  end
 end
