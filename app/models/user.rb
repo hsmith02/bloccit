6074d559cc664ec01903878275ad4de529964e0f
Heather Smith <hsmith.02@outlook.com>
2017-06-07 08:45:06 -0700

    assignment-32 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :favorited_posts, through: :favorites, source: :post
+  has_many :favorited_posts, through: :favorites, source: :post 
 
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
